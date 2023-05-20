1c54e716fd02f551ce1bfa022c6450b4a272ac3b
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-20 16:39:41 -0600

    privacy page setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>test</h1>
+    <main className="">
+      <h1>Block Buster Now</h1>
     </main>
   );
 }
---
+++ app/privacy/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main>
+      <div className="breadCrumb">back to help Home</div>
+      <div className="privacy__container">
+        <h1>Privacy Statement</h1>
+      </div>
+    </main>
+  );
+}
