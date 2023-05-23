323776b4689ab2029de295e8f28c4816e77f1227
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-22 23:20:33 -0600

    update register

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,99 +1,46 @@
-// import bcrypt from "bcrypt";
-// import { NextApiRequest, NextApiResponse } from "next";
-// import prismadb from "@/libs/prismadb";
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   try {
-//     if (req.method !== "POST") {
-//       return res.status(405).end();
-//     }
-
-//     const { email, name, password } = req.body;
-
-//     const existingUser = await prismadb.user.findUnique({
-//       where: {
-//         email,
-//       },
-//     });
-
-//     if (existingUser) {
-//       return res.status(422).json({ error: "Email taken" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 12);
-//     const user = await prismadb.user.create({
-//       data: {
-//         email,
-//         name,
-//         hashedPassword,
-//         image: "",
-//         emailVerified: new Date(),
-//       },
-//     });
-//     return console.log("yes!!!");
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(400).end();
-//   }
-// }
-
 import bcrypt from "bcrypt";
-import { NextApiHandler, NextApiRequest } from "next";
 import prismadb from "@/libs/prismadb";
 
-export const handler = async (req: Request, res: Response) => {
-  if (req.method === "GET") {
-    return new Response("hello from the BOBBY!!!");
-  } else if (req.method === "POST") {
-    // Handle the POST request
-    return new Response("Handling POST request...");
-  } else {
-    return new Response("Method not allowed", { status: 405 });
+export async function GET(req: Request) {
+  return new Response("hello from the sever!!!");
+}
+
+export async function POST(req: Request, res: Response) {
+  try {
+    // if()
+    const body = await req.json();
+    const { email, text, password } = body;
+
+    const existingUser = await prismadb.user.findUnique({
+      where: { email },
+    });
+
+    // Check if user exist
+    if (existingUser) {
+      return new Response(JSON.stringify({ message: "Email Taken" }), {
+        status: 422,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: "",
+        emailVerified: new Date(),
+      },
+    });
+
+    return new Response(JSON.stringify({ user }), { status: 200 });
+
+    // return new Response(console.log(email));
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: `Something went wrong: ${error}` }),
+      { status: 400 }
+    );
   }
-};
-
-// try {
-//   if (req.method !== "POST") {
-//     return res.status(405).end();
-//   }
-
-//   const { email, name, password } = req.body;
-
-//   const existingUser = await prismadb.user.findUnique({
-//     where: {
-//       email,
-//     },
-//   });
-
-//   if (existingUser) {
-//     return res.status(422).json({ error: "Email taken" });
-//   }
-
-//   const hashedPassword = await bcrypt.hash(password, 12);
-//   const user = await prismadb.user.create({
-//     data: {
-//       email,
-//       name,
-//       hashedPassword,
-//       image: "",
-//       emailVerified: new Date(),
-//     },
-//   });
-
-//   console.log("yes!!!");
-//   return res.status(200).end();
-// } catch (err) {
-//   console.log(err);
-//   return res.status(400).end();
-// }
-// };
-
-// export async function GET(req: Request) {
-//   return new Response("hello from the sever!!!");
-// }
-// export async function DELETE(req: Request) {
-//   return new Response("hello from the sever!!!");
-// }
+}
