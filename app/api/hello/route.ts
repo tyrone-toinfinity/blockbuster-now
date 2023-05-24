77cd665c7ddb7a8633201c96850661f4dd4fcd24
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-24 12:42:13 -0600

    fix sign in btn. user auth works!

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,4 +1,3 @@
-export async function POST(request: Request) {
-  const { message } = await request.json(); // Extract the 'message' property from the request body
-  return new Response(`Received message: ${message}`);
+export async function GET(request: Request) {
+  return new Response(`Received message: `);
 }
