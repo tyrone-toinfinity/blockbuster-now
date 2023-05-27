f68c94335c263c5a9f65665b6a83a661ccba7405
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-27 15:22:26 -0600

    movies added to projects

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -85,7 +85,7 @@ const App = () => {
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
         <h1 className="text-3xl md:text-6xl text-white text-center">
-          Who&#39;s watching?
+          Welcome back
         </h1>
         <div className="flex items-center justify-center gap-8 mt-10">
           <div onClick={() => selectProfile()}>
